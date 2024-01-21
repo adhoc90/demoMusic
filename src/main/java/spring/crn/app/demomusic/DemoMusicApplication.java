b0057c92a52e05a0142163fc11eb507673c9e2b8
ad_hoc <ad_hoc90@mail.ru>
2024-01-21 21:49:13 +0300

    initial commit

---
+++ src/main/java/spring/crn/app/demomusic/DemoMusicApplication.java
@@ -0,0 +1,13 @@
+package spring.crn.app.demomusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMusicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoMusicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/spring/crn/app/demomusic/DemoMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.crn.app.demomusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMusicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
